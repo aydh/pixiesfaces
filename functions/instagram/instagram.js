3e783f9fadeda8e0dd85d6b48956c71e031cb838
Andrew Saunders <andrew.saunders@mac.com>
2022-04-10 23:08:57 +1000

    instagram function

---
+++ functions/instagram/instagram.js
@@ -0,0 +1,42 @@
+const process = require('process')
+
+const axios = require('axios')
+const qs = require('qs')
+
+const handler = async function (event) {
+
+  // apply our function to the queryStringParameters and assign it to a variable
+  const params = qs.stringify(event.queryStringParameters)
+  console.log('params', params)
+
+  // Get env var values defined in our Netlify site UI
+  const endpoint = 'https://graph.instagram.com';
+  const userId = process.env.INSTAGRAM_USER_ID;
+  const fields = 'id,caption,media_url,permalink';
+  const token = process.env.INSTAGRAM_ACCESS_TOKEN;
+  const limit = 5;
+  const url = `${endpoint}/${userId}/media/?fields=${fields}&access_token=${token}&count=${limit}`;
+  console.log('Constructed URL is ...', url)
+
+  try {
+    const { data } = await axios.get(url)
+    // refer to axios docs for other methods if you need them
+    // for example if you want to POST data:
+    //    axios.post('/user', { firstName: 'Fred' })
+    return {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  } catch (error) {
+    const { data, headers, status, statusText } = error.response
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    }
+  }
+}
+
+module.exports = { handler }
