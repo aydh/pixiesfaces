f84d21283e28f8402d1bc511f0c08f072af51840
Andrew Saunders <andrew.saunders@mac.com>
2022-04-23 14:11:53 +1000

    Update index.js

--- netlify-plugins/instagram/index.js
+++ netlify-plugins/instagram/index.js
@@ -7,18 +7,18 @@ const chalk   = require('chalk');
 module.exports = {
 
   async onPreBuild({ inputs, utils }) {
-    console.log(chalk.green('Starting up'));
+    console.log('Starting up');
 
     const endpoint = 'https://graph.instagram.com';
     const userId = process.env.INSTAGRAM_USER_ID;
     const fields = 'caption,media_url,media_type,permalink';
     const token = process.env.INSTAGRAM_ACCESS_TOKEN;
     const instagramAPIUrl = `${endpoint}/${userId}/media/?fields=${fields}&access_token=${token}`;
-    console.log(chalk.green('Constructed Instagram API url:', instagramAPIUrl));
+    console.log('Constructed Instagram API url:',  chalk.yellow(instagramAPIUrl));
 
     // Where fetched data should reside in the build
     const dataFile = inputs.dataFile;
-    console.log(chalk.green('Instagram datafile location:', dataFile));
+    console.log('Instagram datafile location:', chalk.yellow(dataFile));
 
     // reinstate from cache if it is present
     let instagramResponse;
@@ -34,10 +34,10 @@ module.exports = {
 
       try {
         const response = await axios.get(instagramAPIUrl)
-        console.log('Intagram Status:', chalk.green(response.status));
+        console.log('Intagram Success - Return Status:', chalk.green(response.status));
         instagramResponse = response.data;
       } catch (err) {
-        console.log('Intagram Status:', chalk.red(err.status));
+        console.log('Intagram Failuew - Return Status:', chalk.red(err.status));
         console.error(err)
       }
 
@@ -60,21 +60,22 @@ module.exports = {
       }
       await fs.writeFileSync(dataFile, JSON.stringify(instagramData));
       await utils.cache.save(dataFile, { ttl: inputs.feedTTL });
-      console.log(chalk.green("Instagram data fetched and cached for:"), chalk.gray(`(TTL:${inputs.feedTTL} seconds)`));
+      console.log(chalk.green("Instagram data fetched and cached"), chalk.gray(`(TTL:${inputs.feedTTL} seconds)`));
     }
 
     // Now we have a well-formated data object describing the instagram feed,
     // let's fetch any uncached images we might need
-    console.log(chalk.green("Iterating over images"));
+    console.log("Iterating over images");
     for (const image in instagramData) {
       let { localImageURL, sourceImageURL } = instagramData[image];
+      console.log("Local image location:", chalk.yellow(localImageURL));
       // if the image exists in the cache, recover it.
       if ( await utils.cache.has(localImageURL) ) {
         await utils.cache.restore(localImageURL);
         console.log('Restored from cache:', chalk.green(localImageURL));
       } else {
         // if the image is not cached, fetch and cache it.
-        console.log("Retrieving image:", chalk.grey(sourceImageURL));
+        console.log("Retrieving from instagram URL:", chalk.grey(sourceImageURL));
         const response = await axios({
           url: sourceImageURL,
           method: 'GET',
