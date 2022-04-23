e30a7640fe43acc7381ee5160ea56d5784d02556
Andrew Saunders <andrew.saunders@mac.com>
2022-04-23 17:13:47 +1000

    reset

--- netlify-plugins/instagram/index.js
+++ netlify-plugins/instagram/index.js
@@ -1,13 +1,15 @@
 const process = require('process');
-const fs      = require('fs');
 const axios   = require('axios');
+const fs      = require('fs');
 const chalk   = require('chalk');
 
 
 module.exports = {
 
   async onPreBuild({ inputs, utils }) {
-    console.log('Starting up');
+    console.log('================================');
+    console.log('= Instagram images starting up =');
+    console.log('================================');
 
     const dataFolder = inputs.dataFolder
     const dataFile = `${dataFolder}/instagram.json`
@@ -17,28 +19,28 @@ module.exports = {
     const fields = 'caption,media_url,media_type,permalink';
     const token = process.env.INSTAGRAM_ACCESS_TOKEN;
     const instagramAPIUrl = `${endpoint}/${userId}/media/?fields=${fields}&access_token=${token}`;
-    console.log('Constructed Instagram API url:',  chalk.yellow(instagramAPIUrl));
+    console.log('Instagram API url comnstructed:',  chalk.yellow(instagramAPIUrl));
     console.log('Instagram data file location:', chalk.yellow(dataFile));
-    console.log('Instagram images location:', chalk.yellow(imageFolder));
+    console.log('Instagram images folder location:', chalk.yellow(imageFolder));
 
     // Create the images directory if it doesn't exist
     if ( await utils.cache.has(imageFolder) ) {
       await utils.cache.restore(imageFolder);
-      console.log('Restored images folder from cache:', chalk.green(imageFolder));
+      console.log('Restored Instagram images folder from cache:', chalk.green(imageFolder));
     } else {
       fs.mkdirSync(imageFolder, { recursive: true });
       await utils.cache.save(imageFolder, { ttl: inputs.imageTTL });
-      console.log('Created images folder:', chalk.green(imageFolder));
+      console.log('Created Instagram images folder:', chalk.green(imageFolder));
     }
 
     // Create the data file directory if it doesn't exist
     if ( await utils.cache.has(dataFolder) ) {
       await utils.cache.restore(dataFolder);
-      console.log('Restored data folder from cache:', chalk.green(dataFolder));
+      console.log('Restored Instagram datafile folder from cache:', chalk.green(dataFolder));
     } else {
       fs.mkdirSync(dataFolder, { recursive: true });
       await utils.cache.save(dataFolder, { ttl: inputs.imageTTL });
-      console.log('Created data folder:', chalk.green(dataFolder));
+      console.log('Created Instagram datafile folder:', chalk.green(dataFolder));
     }
 
     let instagramResponse;
@@ -48,7 +50,7 @@ module.exports = {
     if ( await utils.cache.has(dataFile) ) {
       await utils.cache.restore(dataFile);
       instagramData = require(`${process.cwd()}/${dataFile}`);
-      console.log('Restored from cache:', chalk.green(dataFile));
+      console.log('Instagram datafile restored from cache:', chalk.green(dataFile));
     }
     // Or if it's not cached, let's fetch it and cache it.
     else {
@@ -56,10 +58,10 @@ module.exports = {
 
       try {
         const response = await axios.get(instagramAPIUrl)
-        console.log('Intagram Success - Return Status:', chalk.green(response.status));
+        console.log('Intagram data success - Return status:', chalk.green(response.status));
         instagramResponse = response.data;
       } catch (err) {
-        console.log('Intagram Failure - Return Status:', chalk.red(err.status));
+        console.log('Intagram data failure - Return status:', chalk.red(err.status));
         console.log(err)
       }
 
@@ -69,6 +71,7 @@ module.exports = {
         return;
       }
 
+      // create the instagram JSON and write it out and cache it
       instagramData = [];
       for (const image of instagramResponse.data) {
         let localImageURL = `${imageFolder}/${image.id}.jpg`;
@@ -82,40 +85,43 @@ module.exports = {
       }
       await fs.writeFileSync(dataFile, JSON.stringify(instagramData));
       await utils.cache.save(dataFile, { ttl: inputs.feedTTL });
-      console.log(chalk.green("Instagram data fetched and cached in data file"), chalk.gray(`(TTL:${inputs.feedTTL} seconds)`));
+      console.log(chalk.green("Instagram data fetched and cached in json data file"), chalk.gray(`(TTL:${inputs.feedTTL} seconds)`));
     }
 
     // Now we have a well-formated data object describing the instagram feed,
     // let's fetch any uncached images we might need
-    console.log("Iterating over images");
+    console.log("Iterating over Instagram images");
     for (const image in instagramData) {
       let { localImageURL, sourceImageURL } = instagramData[image];
-      console.log("Local image location:", chalk.yellow(localImageURL));
+      console.log("Instagram image local location:", chalk.yellow(localImageURL));
       // if the image exists in the cache, recover it.
       if ( await utils.cache.has(localImageURL) ) {
-        console.log('Restoring from cache:', chalk.yellow(localImageURL));
+        console.log('Instagram image restoring from cache:', chalk.yellow(localImageURL));
         await utils.cache.restore(localImageURL);
-        console.log('Restored from cache:', chalk.green(localImageURL));
+        console.log('Instagram image restored from cache:', chalk.green(localImageURL));
       } else {
         // if the image is not cached, fetch and cache it.
-        console.log("Retrieving from instagram URL:", chalk.grey(sourceImageURL));
+        console.log("Intagram image retrieving from URL:", chalk.yellow(sourceImageURL));
         try {
           const response = await axios({
             url: sourceImageURL,
             method: 'GET',
             responseType: 'stream'      
           })
-          console.log('Image Retrieval Success - Return Status:', chalk.green(response.status));
+          console.log('Instagram image retrieval success - return status:', chalk.green(response.status));
           const dest = fs.createWriteStream(localImageURL);
           response.data.pipe(dest);
-          console.log("Image written out:", chalk.yellow(localImageURL));
+          console.log("Instagram image written:", chalk.yellow(localImageURL));
           await utils.cache.save(localImageURL, { ttl: inputs.imageTTL });
-          console.log("Image cached:", chalk.green(localImageURL), chalk.gray(`(TTL:${inputs.imageTTL} seconds)`));
+          console.log("Instagram image cached:", chalk.green(localImageURL), chalk.gray(`(TTL:${inputs.imageTTL} seconds)`));
         } catch (err) {
-          console.log('Intagram Failure - Return Status:', chalk.red(err.status));
+          console.log('Instagram image retrieval failure - return status:', chalk.red(err.status));
           console.log(err)
         }
       }
     }
+  console.log('=============================');
+  console.log('= Instagram images finihsed =');
+  console.log('=============================');
   }
 }
