a9ab2e1f0d09dbf217690684b7f8c90340cc3e52
Andrew Saunders <andrew.saunders@mac.com>
2022-09-25 09:05:29 +1000

    good luck

--- netlify-plugins/netlify-plugin-fetch-instagram-basic-api/index.js
+++ netlify-plugins/netlify-plugin-fetch-instagram-basic-api/index.js
@@ -4,6 +4,25 @@ const fs        = require('fs');
 const chalk     = require('chalk');
 const sharp     = require('sharp');
 
+const sharpConv = ( inputFileNamePrefix, size) => {
+
+  let inputFileName = inputFileNamePrefix + '.jpg'
+  let outputFileNameJpg = inputFileNamePrefix + '-' + size + '.jpg';
+  let outputFileNameWebp = inputFileNamePrefix + '-' + size + '.webp';
+
+  sharp(inputFileName)
+    .resize(size, size,{fit: 'cover'})
+    .webp({lossless: true})
+    .toFile(outputFileNameWebp);
+  console.log("Converted",chalk.yellow(inputFileName),"to", chalk.green(outputFileNameWebp));
+
+  sharp(inputFileName)
+    .resize(size, size,{fit: 'cover'})
+    .toFile(outputFileNameJpg);
+  console.log("Converted",chalk.yellow(inputFileName),"to", chalk.green(outputFileNameJpg));
+
+}
+
 module.exports = {
 
   async onPreBuild({ inputs, utils }) {
@@ -59,40 +78,40 @@ module.exports = {
     //else {
     //  console.log('Instagram datafile not present so calling Instagram API');
 
-      try {
-        const response = await axios.get(instagramAPIUrl);
-        console.log('Instagram API success - Return status:', chalk.green(response.status));
-        instagramResponse = response.data;
-      } catch (err) {
-        console.log('Instagram API failure - Return status:', chalk.red(err.status));
-        console.log(err);
-      }
+    try {
+      const response = await axios.get(instagramAPIUrl);
+      console.log('Instagram API success - Return status:', chalk.green(response.status));
+      instagramResponse = response.data;
+    } catch (err) {
+      console.log('Instagram API failure - Return status:', chalk.red(err.status));
+      console.log(err);
+    }
 
-      // If we didn't receive data, fail the plugin but not the build
-      if(!instagramResponse){
-        utils.build.failBuild(`The Instagram feed did not return data.\Halting the build.`);
-        return;
-      }
+    // If we didn't receive data, fail the plugin but not the build
+    if(!instagramResponse){
+      utils.build.failBuild(`The Instagram feed did not return data.\Halting the build.`);
+      return;
+    }
 
-      // create the instagram JSON and write it out and cache it
-      instagramData = [];
-      let i=1;
-      for (const image of instagramResponse.data) {
-        // skip videos
-        if (image.media_type === 'VIDEO') continue;
-        let localImageFilenamePrefix = `${timestamp}-${i}`;
-        instagramData.push({
-          "id": image.id,
-          "caption": image.caption,
-          "media_type": image.media_type,
-          "instagramURL": image.permalink,
-          "sourceImageURL": image.media_url,
-          "localImageFilenamePrefix": localImageFilenamePrefix
-        });
-        i++
-      }
-      await fs.writeFileSync(dataFile, JSON.stringify(instagramData));
-      console.log("Instagram data fetched and written to json data file ",chalk.green(dataFile));
+    // create the instagram JSON and write it out and cache it
+    instagramData = [];
+    let i=1;
+    for (const image of instagramResponse.data) {
+      // skip videos
+      if (image.media_type === 'VIDEO') continue;
+      let localImageFilenamePrefix = `${timestamp}-${i}`;
+      instagramData.push({
+        "id": image.id,
+        "caption": image.caption,
+        "media_type": image.media_type,
+        "instagramURL": image.permalink,
+        "sourceImageURL": image.media_url,
+        "localImageFilenamePrefix": localImageFilenamePrefix
+      });
+      i++
+    }
+    await fs.writeFileSync(dataFile, JSON.stringify(instagramData));
+    console.log("Instagram data fetched and written to json data file ",chalk.green(dataFile));
     //  await utils.cache.save(dataFile, { ttl: inputs.feedTTL });
     //  console.log(chalk.green("Instagram data fetched and cached in json data file"), chalk.gray(`(TTL:${inputs.feedTTL} seconds)`));
     //}
@@ -104,16 +123,6 @@ module.exports = {
     for (const image in instagramData) {
       let { localImageFilenamePrefix, sourceImageURL } = instagramData[image];
       let localImageJpg = `${imageFolder}/${localImageFilenamePrefix}.jpg`;
-
-      let localImageSmallJpg = `${imageFolder}/${localImageFilenamePrefix}-small.jpg`;
-      let localImageMediumJpg = `${imageFolder}/${localImageFilenamePrefix}-medium.jpg`;
-      let localImageLargeJpg = `${imageFolder}/${localImageFilenamePrefix}-large.jpg`;
-
-      let localImageWebp = `${imageFolder}/${localImageFilenamePrefix}.webp`;
-
-      let localImageSmallWebp = `${imageFolder}/${localImageFilenamePrefix}-small.webp`;
-      let localImageMediumWebp = `${imageFolder}/${localImageFilenamePrefix}-medium.webp`;
-      let localImageLargeWebp = `${imageFolder}/${localImageFilenamePrefix}-large.webp`;
 
       //console.log("Instagram image local filename:", chalk.yellow(localImageURL));
       // if the image exists in the cache, recover it.
@@ -131,49 +140,16 @@ module.exports = {
             responseType: 'stream'      
           });          
           //console.log('Instagram image retrieval success - return status:', chalk.green(response.status));
-          const dest = fs.createWriteStream(localImageJpg);
-          response.data.pipe(dest,{emitClose: true});
-          console.log("Processing image #",j);
-          await dest.on('finish', () => {
-            console.log("Image written to:", chalk.green(localImageJpg));
-
-            sharp(localImageJpg)
-              .resize(480, 480,{fit: 'cover'})
-              .toFile(localImageSmallJpg);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageSmallJpg));
-
-            sharp(localImageJpg)
-              .resize(768, 768,{fit: 'cover'})
-              .toFile(localImageMediumJpg);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageMediumJpg));
-
-            sharp(localImageJpg)
-              .resize(1024, 1024,{fit: 'cover'})
-              .toFile(localImageLargeJpg);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageLargeJpg));
-
-            sharp(localImageJpg)
-              .webp({lossless: true})
-              .toFile(localImageWebp);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageWebp));
-
-            sharp(localImageJpg)
-              .resize(480, 480,{fit: 'cover'})
-              .webp({lossless: true})
-              .toFile(localImageSmallWebp);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageSmallWebp));
-
-            sharp(localImageJpg)
-              .resize(768, 768,{fit: 'cover'})
-              .webp({lossless: true})
-              .toFile(localImageMediumWebp);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageMediumWebp));
-
-            sharp(localImageJpg)
-              .resize(1024, 1024,{fit: 'cover'})
-              .webp({lossless: true})
-              .toFile(localImageLargeWebp);
-            console.log("Converted",chalk.yellow(localImageJpg),"to", chalk.green(localImageLargeWebp));
+        const dest = fs.createWriteStream(localImageJpg);
+        response.data.pipe(dest,{emitClose: true});
+        console.log("Processing image #",j);
+        await dest.on('finish', () => {
+          console.log("Image written to:", chalk.green(localImageJpg));
+          sharpConv(localImageFilenamePrefix,360);
+          sharpConv(localImageFilenamePrefix,720);
+          sharpConv(localImageFilenamePrefix,1024);
+          sharpConv(localImageFilenamePrefix,1250);
+          sharpConv(localImageFilenamePrefix,1440);
         });
           //await utils.cache.save(localImageURL, { ttl: inputs.imageTTL });
           //console.log("Instagram image cached:", chalk.green(localImageURL), chalk.gray(`(TTL:${inputs.imageTTL} seconds)`));
@@ -184,11 +160,13 @@ module.exports = {
         j++;
       //}
  
+    }
+    // Wait so we can allow the async bits to complete - I think this is cos mixing sync/async.
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log(chalk.cyanBright("============================="));
+    console.log(chalk.cyanBright("= Instagram images finished ="));
+    console.log(chalk.cyanBright("============================="));
   }
-  // Wait so we can allow the async bits to complete - I think this is cos mixing sync/async.
-  await new Promise(resolve => setTimeout(resolve, 5000));
-  console.log(chalk.cyanBright("============================="));
-  console.log(chalk.cyanBright("= Instagram images finished ="));
-  console.log(chalk.cyanBright("============================="));
-  }
+
+
 }
